237b5285036dc80c3731af484c32ef515eda95a3
Seth Ladd <sethladd@sethladd-macbookpro.local>
2012-02-23 22:34:51 -0800

    first pass at local storage

--- lawndart.dart
+++ lawndart.dart
@@ -22,6 +22,7 @@ interface Store<K, V> {
   Future<bool> exists(K key);
   Future<Collection<V>> all();
   Future<bool> removeByKey(K key);
+  // TODO: what are the semantics of bool here?
   Future<bool> removeByKeys(Collection<K> _keys);
   Future<bool> nuke();
 }
@@ -108,13 +109,15 @@ class LocalStorageAdapter<K extends String, V> implements Adapter<K, V> {
     storage = window.localStorage;
   }
   
+  List<K> get _allKeys() => JSON.parse(storage.getItem(INDEX_KEY));
+  
   Future<Collection<K>> keys() {
-    return _results(JSON.parse(storage.getItem(INDEX_KEY)));
+    return _results(_allKeys);
   }
   
   Future<K> save(V obj, [K key]) {
     key = key == null ? _uuid() : key;
-    storage.setItem(key, JSON.stringify(data));
+    storage.setItem(key, JSON.stringify(obj));
     return _results(key);
   }
   
@@ -123,9 +126,9 @@ class LocalStorageAdapter<K extends String, V> implements Adapter<K, V> {
     for (var i = 0; i < objs.length; i++) {
       K key = _keys[i];
       key = key == null ? _uuid() : key;
-      storage.setItem(key, JSON.stringify(obj));
+      storage.setItem(key, JSON.stringify(objs[i]));
     }
-    return _result(newKeys);
+    return _results(newKeys);
   }
   
   Future<V> getByKey(K key) {
@@ -142,19 +145,26 @@ class LocalStorageAdapter<K extends String, V> implements Adapter<K, V> {
   }
   
   Future<Collection<V>> all() {
-    var values = keys().map((key) => storage.getItem(key));
+    var values = _allKeys.map((key) => storage.getItem(key));
     return _results(values);
   }
   
   Future<bool> removeByKey(K key) {
-    List<K> _keys = keys();
-    if (_keys.indexOf(key) > -1) {
-      return _results(true);
-    } else {
-      return _results(false);
-    }
+    List<K> _keys = _allKeys;
+    _keys.removeRange(_keys.indexOf(key), 1);
+    storage.removeItem(key);
+    storage.setItem(INDEX_KEY, JSON.stringify(_keys));
+    return _results(true);
   }
   
-  Future<bool> removeByKeys(Collection<K> _keys);
-  Future<bool> nuke();
+  Future<bool> removeByKeys(Collection<K> _keys) {
+    _keys.forEach((key) => removeByKey(key));
+    return _results(true);
+  }
+  
+  Future<bool> nuke() {
+    storage.removeItem(INDEX_KEY);
+    storage.clear();
+    return _results(true);
+  }
 }
