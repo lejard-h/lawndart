27e6556843ec4e401d5f9d3bc7202bab7d6aff1d
Seth Ladd <sethladd@gmail.com>
2013-02-12 23:22:44 -0800

    more work on websql

--- lib/websql_adapter.dart
+++ lib/websql_adapter.dart
@@ -17,13 +17,14 @@ part of lawndart;
 class WebSqlAdapter<K, V> extends Store<K, V> {
   
   static final String VERSION = "1";
+  static const int INITIAL_SIZE = 5 * 1024 * 1024;
   
   String dbName;
   String storeName;
   int estimatedSize;
   Database _db;
   
-  WebSqlAdapter(this.dbName, this.storeName, {this.estimatedSize: 5 * 1024 * 1024});
+  WebSqlAdapter(this.dbName, this.storeName, {this.estimatedSize: INITIAL_SIZE});
   
   Future<bool> open() {
     var completer = new Completer();
@@ -37,25 +38,23 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
 
     _db.transaction((txn) {
       txn.executeSql(sql, [], (txn, resultSet) {
-        isReady = true;
+        _isOpen = true;
         completer.complete(true);
       });
     }, (error) => completer.completeError(error));
   }
   
   // TODO
-  Future<Collection<K>> keys() {
+  Future<Collection<K>> _keys() {
     throw new UnimplementedError();
   }
   
   Future _save(V obj, K key) {
-    String value = JSON.stringify(obj);
-    var completer = new Completer<K>();
-
+    var completer = new Completer();
     var upsertSql = 'INSERT OR REPLACE INTO $storeName (id, value) VALUES (?, ?)';
     
     _db.transaction((txn) {
-      txn.executeSql(upsertSql, [key, value], (txn, resultSet) {
+      txn.executeSql(upsertSql, [key, obj], (txn, resultSet) {
         completer.complete(key);
       });
     }, (error) => completer.completeError(error));
@@ -64,8 +63,7 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
   }
   
   Future<V> _getByKey(K key) {
-    var completer = new Completer<V>();
-    
+    var completer = new Completer();
     var sql = 'SELECT value FROM $storeName WHERE id = ?';
 
     _db.readTransaction((txn) {
@@ -74,7 +72,7 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
         if (row == null) {
           completer.complete(null);
         } else {
-          completer.complete(JSON.parse(row['value']));
+          completer.complete(row['value']);
         }
       });
     }, (error) => completer.completeError(error));
@@ -82,70 +80,60 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
     return completer.future;
   }
   
-  Future<bool> _removeByKey(K key) {
-    var completer = new Completer<bool>();
-    
+  Future _removeByKey(K key) {
+    var completer = new Completer();
     var sql = 'DELETE FROM $storeName WHERE id = ?';
 
     _db.transaction((txn) {
       txn.executeSql(sql, [key], (txn, resultSet) {
-        if (resultSet.rowsAffected < 0) {
-          completer.complete(false);
-        } else {
-          completer.complete(true);
-        }
+        // maybe later, if (resultSet.rowsAffected < 0)
+        completer.complete(true);
       });
     }, (error) => completer.completeError(error));
     
     return completer.future;
   }
   
-  Future<bool> _nuke() {
-    Completer<bool> completer = new Completer<bool>();
+  Future _nuke() {
+    var completer = new Completer();
     
 //    var sql = 'TRUNCATE TABLE $storeName';
     var sql = 'DELETE FROM $storeName';
     _db.transaction((txn) {
-      txn.executeSql(sql, [], 
-          (txn, resultSet) => completer.complete(true), 
-          _onError);
-    });
+      txn.executeSql(sql, [],  (txn, resultSet) => completer.complete(true));
+    }, (error) => completer.completeError(error));
     return completer.future;
   }
   
-  Future _all() {  
+  Future<Iterable<V>> _all() {  
     var sql = 'SELECT id,value FROM $storeName';
-
-    Completer<Collection<V>> completer = new Completer<Collection<V>>();
-    var values = [];    
+    var completer = new Completer<Collection<V>>();
+    var values = new Queue<V>();    
     _db.transaction((txn) {
       txn.executeSql(sql, [], (txn, resultSet) {
         for (var each in resultSet.rows) {
           values.add(each['value']);
         }
         completer.complete(values);
-      }, _onError);
-    });
+      });
+    }, (error) => completer.completeError(error));
     
     return completer.future;
   }
   
   Future _batch(Map<K, V> objs) {
     var completer = new Completer();
-
     var upsertSql = 'INSERT OR REPLACE INTO $storeName (id, value) VALUES (?, ?)';
     
     _db.transaction((txn) {
-      for (var key in objs.keys) {
-        V obj = objs[key];
-        var value = JSON.stringify(obj);
-        txn.executeSql(upsertSql, [key, value]);
-      }
-    },
-    (error) => completer.completeError(error),
-    () { // on success
-      completer.complete(true);
-    });
+        for (var key in objs.keys) {
+          V obj = objs[key];
+          txn.executeSql(upsertSql, [key, obj]);
+        }
+      },
+      (error) => completer.completeError(error),
+      () => completer.complete(true)
+    );
     
     return completer.future;
   }
@@ -154,7 +142,7 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
     var sql = 'SELECT value FROM $storeName WHERE id = ?';
 
     var completer = new Completer<Iterable<V>>();
-    var values = <V>[];
+    var values = new Queue<V>();
     
     _db.transaction((txn) {
       _keys.forEach((key) {
@@ -169,9 +157,8 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
     return completer.future;
   }
 
-  Future<bool> _removeByKeys(Iterable<K> _keys) {
-    var sql = 'DELETE FROM $storeName WHERE id = ?';
-    
+  Future _removeByKeys(Iterable<K> _keys) {
+    var sql = 'DELETE FROM $storeName WHERE id = ?'; 
     var completer = new Completer<bool>();
     
     _db.transaction((txn) {
@@ -185,15 +172,5 @@ class WebSqlAdapter<K, V> extends Store<K, V> {
     
     return completer.future;
   }
-  
-  bool _onError(SqlTransaction transaction, SqlError error){
-    print('Database error: ${error.code} ${error.message}');
-    return true;
-  }
-  /*
 
-  Future<bool> exists(K key);
-  Future<Collection<K>> keys()
-  
-  */
 }
