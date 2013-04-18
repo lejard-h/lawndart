55758b4f186a082a664f3428b3d18624977932aa
David Bernard <david.bernard.31@gmail.com>
2013-04-18 17:13:47 +0200

    sdk update: 0.4.7+1

--- lib/src/websql_store.dart
+++ lib/src/websql_store.dart
@@ -19,24 +19,24 @@ part of lawndart;
  * WebSQL is a transactional database.
  */
 class WebSqlStore<V> extends Store<V> {
-  
+
   static final String VERSION = "1";
   static const int INITIAL_SIZE = 5 * 1024 * 1024;
-  
+
   String dbName;
   String storeName;
   int estimatedSize;
   SqlDatabase _db;
-  
+
   WebSqlStore(this.dbName, this.storeName, {this.estimatedSize: INITIAL_SIZE});
-  
+
   /// Returns true if WebSQL is supported on this platform.
   static bool get supported => SqlDatabase.supported;
-  
+
   @override
   Future<bool> open() {
     if (!supported) {
-      return new Future.immediateError(
+      return new Future.error(
         new UnsupportedError('WebSQL is not supported on this platform'));
     }
     var completer = new Completer();
@@ -44,7 +44,7 @@ class WebSqlStore<V> extends Store<V> {
     _initDb(completer);
     return completer.future;
   }
-  
+
   void _initDb(Completer completer) {
     var sql = 'CREATE TABLE IF NOT EXISTS $storeName (id NVARCHAR(32) UNIQUE PRIMARY KEY, value TEXT)';
 
@@ -55,7 +55,7 @@ class WebSqlStore<V> extends Store<V> {
       });
     }, (error) => completer.completeError(error));
   }
-  
+
   @override
   Stream<String> _keys() {
     var sql = 'SELECT id FROM $storeName';
@@ -70,29 +70,29 @@ class WebSqlStore<V> extends Store<V> {
     },
     (error) => controller.addError(error),
     () => controller.close());
-    
+
     return controller.stream;
   }
-  
+
   @override
   Future _save(V obj, String key) {
     var completer = new Completer();
     var upsertSql = 'INSERT OR REPLACE INTO $storeName (id, value) VALUES (?, ?)';
-    
+
     _db.transaction((txn) {
       txn.executeSql(upsertSql, [key, obj], (txn, resultSet) {
         completer.complete(key);
       });
     }, (error) => completer.completeError(error));
-    
+
     return completer.future;
   }
-  
+
   @override
   Future<bool> _exists(String key) {
     return _getByKey(key).then((v) => v != null);
   }
-  
+
   @override
   Future<V> _getByKey(String key) {
     var completer = new Completer();
@@ -108,10 +108,10 @@ class WebSqlStore<V> extends Store<V> {
         }
       });
     }, (error) => completer.completeError(error));
-    
+
     return completer.future;
   }
-  
+
   @override
   Future _removeByKey(String key) {
     var completer = new Completer();
@@ -123,23 +123,23 @@ class WebSqlStore<V> extends Store<V> {
         completer.complete(true);
       });
     }, (error) => completer.completeError(error));
-    
+
     return completer.future;
   }
-  
+
   @override
   Future _nuke() {
     var completer = new Completer();
-    
+
     var sql = 'DELETE FROM $storeName';
     _db.transaction((txn) {
       txn.executeSql(sql, [], (txn, resultSet) => completer.complete(true));
     }, (error) => completer.completeError(error));
     return completer.future;
   }
-  
+
   @override
-  Stream<V> _all() {  
+  Stream<V> _all() {
     var sql = 'SELECT id,value FROM $storeName';
     var controller = new StreamController<V>();
     _db.transaction((txn) {
@@ -152,15 +152,15 @@ class WebSqlStore<V> extends Store<V> {
     },
     (error) => controller.addError(error),
     () => controller.close());
-    
+
     return controller.stream;
   }
-  
+
   @override
   Future _batch(Map<String, V> objs) {
     var completer = new Completer();
     var upsertSql = 'INSERT OR REPLACE INTO $storeName (id, value) VALUES (?, ?)';
-    
+
     _db.transaction((txn) {
         for (var key in objs.keys) {
           V obj = objs[key];
@@ -170,7 +170,7 @@ class WebSqlStore<V> extends Store<V> {
       (error) => completer.completeError(error),
       () => completer.complete(true)
     );
-    
+
     return completer.future;
   }
 
@@ -179,7 +179,7 @@ class WebSqlStore<V> extends Store<V> {
     var sql = 'SELECT value FROM $storeName WHERE id = ?';
 
     var controller = new StreamController<V>();
-    
+
     _db.transaction((txn) {
       _keys.forEach((key) {
         txn.executeSql(sql, [key], (txn, resultSet) {
@@ -191,15 +191,15 @@ class WebSqlStore<V> extends Store<V> {
     },
     (error) => controller.addError(error),
     () => controller.close());
-    
+
     return controller.stream;
   }
 
   @override
   Future _removeByKeys(Iterable<String> _keys) {
-    var sql = 'DELETE FROM $storeName WHERE id = ?'; 
+    var sql = 'DELETE FROM $storeName WHERE id = ?';
     var completer = new Completer<bool>();
-    
+
     _db.transaction((txn) {
       _keys.forEach((key) {
         // TODO verify I don't need to do anything in the callback
@@ -208,7 +208,7 @@ class WebSqlStore<V> extends Store<V> {
     },
     (error) => completer.completeError(error),
     () => completer.complete(true));
-    
+
     return completer.future;
   }
 
